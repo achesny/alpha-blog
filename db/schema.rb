d9a1066f8f9291b8701d9f878bf1c43ce238375a
Aaron Chesny <achesny@gmail.com>
2024-05-01 11:07:45 -0400

    Added password to user

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_04_24_195023) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_01_150105) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -23,6 +24,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_04_24_195023) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
 end
